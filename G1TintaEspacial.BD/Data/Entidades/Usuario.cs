7123768a344f98f869c10b9e519d6f7750a78c04
duran.valentina@hotmail.com <duran.valentina@hotmail.com>
2022-09-04 22:39:31 -0300

    Agregar archivos de proyecto.

---
+++ G1TintaEspacial.BD/Data/Entidades/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TintaEspacial.BD.data.comun;
+using TINTAESPACIAL.DataBase.Entidades;
+
+namespace TINTAESPACIAL.DataBase.data.Entidades
+{
+    public class Usuario : UsuarioBase
+    {
+        
+        
+    }
+}
---
+++ G1TintaEspacial.BD/Data/dbcontex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TINTAESPACIAL.DataBase.data.Entidades;
+using TINTAESPACIAL.DataBase.Entidades;
+
+namespace TINTAESPACIAL.DataBase
+{ 
+    public class dbcontex: DbContext
+    {
+        public DbSet<UsuarioTatuador> Tatuadores { get; set; }
+        public DbSet<UsuarioArtista> Artistas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        
+        public DbSet<UsuarioEstudio> UsuarioEstudios { get; set; }
+        public DbSet<UsuarioNFT> UsuarioNFTs { get; set; }
+
+        public dbcontex(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+    }
+}
