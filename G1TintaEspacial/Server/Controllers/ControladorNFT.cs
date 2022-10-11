081b6f4058b2762702000d62534ff9aa6b0dfd25
duran.valentina@hotmail.com <duran.valentina@hotmail.com>
2022-10-10 22:26:20 -0300

    detalles3

--- G1TintaEspacial/Server/Controllers/ControladorNFT.cs
+++ G1TintaEspacial/Server/Controllers/ControladorNFT.cs
@@ -4,7 +4,7 @@ using TINTAESPACIAL.DataBase;
 using G1TintaEspacial.BD.Data.Entidades;
 
 namespace G1TintaEspacial.Server.Controllers
-{   
+{
     [ApiController]
     [Route("api/NFT")]
     public class ControladorNFT : ControllerBase
@@ -15,10 +15,10 @@ namespace G1TintaEspacial.Server.Controllers
             this.contex = contex;
         }
         [HttpGet]
-         public async Task<ActionResult<List<NFT>>> Get()
-         {
-                return await contex.NFTs.ToListAsync();
-         }
+        public async Task<ActionResult<List<NFT>>> Get()
+        {
+            return await contex.NFTs.ToListAsync();
+        }
         #region GET ID
         [HttpGet("id:int")]
         public async Task<ActionResult<NFT>> Get(int id)
@@ -31,13 +31,76 @@ namespace G1TintaEspacial.Server.Controllers
             return NFT;
         }
         #endregion
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(NFT nft)
+        {
+            try
+            {
+                contex.NFTs.Add(nft);
+                await contex.SaveChangesAsync();
+                return nft.Id;
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, [FromBody] NFT nFT)
+        {
+            if (id != nFT.Id)
+            {
+                return BadRequest("Datos incorrectos");
+            }
+
+            var mati = contex.NFTs.Where(e => e.Id == id).FirstOrDefault();
+            if (mati == null)
+            {
+                return NotFound("No existe el NFT a modificar");
+            }
+
+            mati.Id = nFT.Id;
+            mati.NombreObra = nFT.NombreObra;
+            mati.Descripcion = nFT.Descripcion;
+            mati.Autor = nFT.Autor;
+            mati.Precio = nFT.Precio;
+            mati.Token = nFT.Token;
+            mati.Precio = nFT.Precio;
+            try
+            {
+                //throw(new Exception("Cualquier Verdura"));
+                contex.NFTs.Update(mati);
+                contex.SaveChanges();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Los datos no han sido actualizados por: {e.Message}");
+            }
+        }
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var mati = contex.NFTs.Where(x => x.Id == id).FirstOrDefault();
+
+            if (mati == null)
+            {
+                return NotFound($"El registro {id} no fue encontrado");
+            }
+            try
+            {
+                contex.NFTs.Remove(mati);
+                contex.SaveChanges();
+                return Ok($"El registro de {mati.Token} ha sido borrado.");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Los datos no pudieron eliminarse por: {e.Message}");
+            }
+        }
+
     }
-
-
-
-
-
-
 
 }
 
